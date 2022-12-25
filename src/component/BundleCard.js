fea05b370c3ffc4c0eb77bb5f4a508410129eae9
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-25 11:40:30 +0300

    nbSubscribed >= 0

--- src/component/BundleCard.js
+++ src/component/BundleCard.js
@@ -630,7 +630,7 @@ function BundleCard({ data }) {
           component="h6"
           style={{ color: "#000", fontWeight: "400", fontSize: "12px" }}
         >
-          {nbSubscribed ? nbSubscribed > 0 ? nbSubscribed+" members subscribed"  : "0 subscriber" : ""}
+          {nbSubscribed ? nbSubscribed >= 0 ? nbSubscribed+" members subscribed"  : "0 subscriber" : ""}
         </Typography>
         
         <Box className={classes.buttonGroup}>
