de5a0424ce1acad54e5fcdcf75bb84cd25b5915b
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-26 11:17:56 +0300

    Update UserDetailsCard.js

--- src/component/UserDetailsCard.js
+++ src/component/UserDetailsCard.js
@@ -199,7 +199,7 @@ export default function UserDetailsCard( data ) {
                 variant="h4"
                 component="h4"
                 style={{
-                  color: "#fbfafa",
+                  color: "#444",
                   cursor: "pointer",
                   textOverflow: "ellipsis",
                   whiteSpace: "nowrap",
