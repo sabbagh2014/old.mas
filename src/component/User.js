cdab7f7c4d491038cba7c26aafc0ca942f491889
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-26 11:04:09 +0300

    Update User.js

--- src/component/User.js
+++ src/component/User.js
@@ -1,254 +1,89 @@
-import React, { useContext, useState, useEffect } from "react";
-import { Typography, Box, Avatar, Button, Card, makeStyles } from "@material-ui/core";
-import { useNavigate } from "react-router-dom";
-import { FaHeart } from "react-icons/fa";
-import axios from "axios";
-import Apiconfigs from "src/Apiconfig/Apiconfigs";
-import { UserContext } from "src/context/User";
-import { toast } from "react-toastify";
+import React from "react";
+import {
+  Grid,
+  Box,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { sortAddress } from "src/utils";
 
-const useStyles = makeStyles((theme) => ({
-  cards: {
-    backgroundColor: '#D9AFD9',
-    backgroundImage: 'linear-gradient(0deg, #D9AFD9 0%, #97D9E1 100%)',
-    padding: "10px",
-    borderRadius: "10px",
-    width: "260px",
-    minHeight: "320px",
-    margin: "12px",
-    position: "relative",
-    display: 'flex',
-    flexDirection: "column",
-    position: 'relative',
-    alignItems: "stretch",
-    justifyContent: 'flex-end',
-  },
-  cardContent: {
-    backgroundImage: 'linear-gradient(0deg, #D9AFD955 0%, #97D9E199 100%)',
-    borderRadius:"10px",
-    padding:"10px",
+const useStyles = makeStyles(() => ({
+  borderBotm: {
+    borderBottom: "1px solid #ccc",
     display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    textAlign: "center",
-    position: "relative",
-    backdropFilter: "blur(3px)",
-    "& h6": {
-      marginBottom: "2px !important",
-      fontSize: "15px !important",
-      [theme.breakpoints.down("md")]: {
-        fontSize: "10px !important",
-      },
-      "& span": {
-        color: "#000",
-        paddingLeft: "5px",
-      },
-      "@media(max-width:821px)": {
-        fontSize: "11px !important",
-      },
-    },
-    "& p": {
-      fontSize: "12px",
-    },
+    background: "#fff",
+    width: "275px",
   },
-
-  boxActions: {
-    padding: '4px',
-    marginTop: '4px',
-    justifyContent: "space-between",
-    alignItems: "center",
-    display: "flex",
-    width: "100%",
-    borderRadius: "4px",
-    background: "#fff4",
-    backdropFilter: "blur(2px)",
-    alignSelf: "flex-end",
-    zIndex:100
-  },
-
-  subButton: {
-    "& button": {
-      backgroundImage: "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)",
-      border: 0,
-      borderRadius: 3,
-      boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-      color: "#fff",
-      width: "90px",
-      height: "30px",
-      fontSize: "11px",
-      padding: "2px"
-    },
+  
+  main: {
+    color: "#999",
+    background: "#fff",
+    cursor: "pointer",
+    padding: "10px"
   },
 }));
 
-export default function UserDetailsCard(data) {
-  const userCardData = data.data;
-  const navigate = useNavigate();
+export default function UsersCard({
+  users,
+  navigate,
+  setsearch,
+  setIsLoading1,
+}) {
   const classes = useStyles();
-  const auth = useContext(UserContext);
-
-  const [isLike, setisLike] = useState(false);
-  const [nbLike, setnbLike] = useState(0);
-  const [isSubscribed, setisSubscribed] = useState(false);
-  const [nbSubscribed, setnbSubscribed] = useState(0);
-
-  const subscribeToUserHandler = async () => {
-    if (auth.userData?._id) {
-    await axios({
-      method: "GET",
-      url: Apiconfigs.followProfile + userCardData._id,
-      headers: {
-        token: sessionStorage.getItem("token"),
-      },
-    })
-      .then(async (res) => {
-        if (res.data.statusCode === 200) {
-          setisSubscribed(res.data.result.subscribed == "yes");
-          setnbSubscribed(res.data.result.nb);
-        } else {
-          toast.error(res.data.result);
-        }
-      })
-      .catch((err) => {
-        toast.error(err?.response?.data?.responseMessage);
-      });
-    } else {
-      toast.error("Please Login");
-    }
-  }
-
-  const likeDislikeUserHandler = async (id) => {
-    if (auth.userData?._id) {
-      try {
-        const res = await axios.get(Apiconfigs.likeDislikeUser + id, {
-          headers: {
-            token: sessionStorage.getItem("token"),
-          },
-        });
-        if (res.data.statusCode === 200) {
-          setisLike((liked) => !liked);
-          setnbLike((nb) => isLike ? nb - 1 : nb + 1)
-        } else {
-          setisLike(false);
-          toast.error(res.data.responseMessage);
-        }
-      } catch (error) {
-        console.log(error)
-      }
-
-    } else {
-      toast.error("Please Login");
-    }
+  const handleCloseFunction = () => {
+    navigate("/user-profile/"+users.userName)
+    setIsLoading1(false);
+    setsearch("");
   };
 
-  useEffect(() => {
-    setnbLike(userCardData.likesUsers.length);
-    setnbSubscribed(userCardData.followers.length);
-    if (auth.userData?._id) {
-      setisLike(userCardData.likesUsers?.includes(auth.userData._id));
-      setisSubscribed(userCardData.followers?.includes(auth.userData._id));
-    }
-  }, [])
-
   return (
-    <Card className={classes.cards}>
-      <img
-            style={{zIndex:'0', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', margin: 'auto', height: '100%'}}
-           onClick={() => {
-              navigate("/user-profile/" + userCardData.userName)
-            }}
-            src={userCardData.profilePic} />
-      <Box className={classes.cardContent}>
-        
-        <Box style={{
-          textAlign: "center",
-          justifyContent: "center",
-          alignItems: "center",
-          display: "flex",
-          flex: 1,
-          flexGrow: 1,
-          flexDirection: "column",
-        }}>
-          
-          <Box
-           
-          >
-            <Typography
-              variant="h4"
-              component="h4"
-              style={{
-                color: "#fbfafa",
-                cursor: "pointer",
-                textOverflow: "ellipsis",
-                whiteSpace: "nowrap",
-                width: "auto",
-                textAlign: "center"
-              }}
+    <Box className={classes.main} onClick={handleCloseFunction}>
+      <Grid 
+        container
+        direction="row"
+        justifyContent="flex-start"
+        alignItems="stretch"
+        spacing={2}
+      >
+        <Grid item >
+          <Box>
+            <figure
+              style={{ cursor: "pointer", marginLeft: "10px" }}
             >
-              {userCardData && userCardData.name
-                ? userCardData.name
-                : userCardData.userName}
-            </Typography><Box>
-              {
-                userCardData.speciality &&
-                <Typography
-                  style={{
-                    color: "#000",
-                    fontWeight: "700",
-                    textAlign: "center",
-                    fontSize: "12px"
-                  }}
-                >
-                  {userCardData.speciality}
-                </Typography>
-              }
+              <img
+                src={users.profilePic || `https://avatars.dicebear.com/api/miniavs/${users?.userName}.svg`}
+                style={{ height: "45px", width: "45px" }}
+              />
+            </figure>
+            <Typography
+              variant="h5"
+              component="h4"
+              style={{ color: "black" }}
+            >
+              {users.name
+                ? users.name
+                : users.userType === "User"
+                ? sortAddress(users.walletAddress)
+                : sortAddress(users.ethAccount.address)}
+            </Typography>
+          </Box>
+        </Grid>
+
+        <Grid item >
+          <Box>
+            <Box>
+              <Typography variant="h6" component="h6">
+                {users.followers.length}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="span" component="span">
+                subscribers
+              </Typography>
             </Box>
           </Box>
-        </Box>
-        
-      </Box>
-      
-      <Box
-          className={classes.boxActions}
-        >
-          <Box className={classes.subButton}>
-            
-                <Button onClick={subscribeToUserHandler}>
-                  {isSubscribed ? 'Subscribed' : 'Subscribe'}
-                </Button>
-          
-            <span
-              style={{
-                color: "#000",
-                fontWeight: "600",
-                fontSize: "12px",
-                padding: '2px'
-              }}
-            >
-              {
-                nbSubscribed ?
-                  nbSubscribed > 0 ?
-                    nbSubscribed + " subs" :
-                    '0 sub' : "0 sub"
-              }
-            </span>
-          </Box>
-          <Box style={{
-            cursor: "pointer",
-            margin: "0 3px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }} >
-            <span style={{ fontSize: '12px', margin: "1px 2px" }}>
-              {nbLike && nbLike}
-            </span>
-            <FaHeart
-              style={isLike ? { color: "red" } : { color: "#ffa0a0" }}
-              onClick={() => likeDislikeUserHandler(userCardData._id)}
-            />
-          </Box>  
-      </Box>
-    </Card>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
